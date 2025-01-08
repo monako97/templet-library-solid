3e42b65fddadbb08f27561f068f15f2df5280bd1
moneko97 <poi.nyaa@qq.com>
2025-01-08 22:46:21 +0800

    update

--- config/index.ts
+++ config/index.ts
@@ -1,18 +1,19 @@
 import type { ConfigType } from 'PackageNameByCore';
 
+const CDNHOST = 'https://cdn.statically.io';
 const conf: Partial<ConfigType> = {
   htmlPluginOption: {
     favicon: './site/assets/images/favicon.ico',
     meta: {
       CSP: {
         'http-equiv': 'Content-Security-Policy',
-        content: "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
+        content: `script-src 'self' ${CDNHOST} 'unsafe-eval' 'unsafe-inline' blob:;`,
       },
     },
     tags: [
       {
         tag: 'script',
-        src: 'https://cdn.statically.io/gh/monako97/cdn/main/npm/%40webcomponents/webcomponentsjs/2.8.0/webcomponents-loader.js',
+        src: CDNHOST + '/gh/monako97/cdn/main/npm/%40webcomponents/webcomponentsjs/2.8.0/webcomponents-loader.js',
       },
     ],
   },
--- typings/typings.d.ts
+++ typings/typings.d.ts
@@ -1,5 +1,4 @@
 declare module 'libraryNameTemplate' {
-  // eslint-disable-next-line import/no-unresolved
   export * from 'components';
 }
 declare module '@pkg';
